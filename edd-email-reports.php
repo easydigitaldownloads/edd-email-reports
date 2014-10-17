b2dbfe166fdaf7c97725ce06651edb2eb67feefa
Dave Kiss <hello@davekiss.com>
2014-10-16 21:28:31 -0400

    Add a default timezone for sites that do not have the timezone defined

--- edd-email-reports.php
+++ edd-email-reports.php
@@ -159,7 +159,8 @@ if( !class_exists( 'EDD_Email_Reports' ) ) {
         public function schedule_daily_email() {
           if ( ! wp_next_scheduled( 'edd_email_reports_daily_email' ) && ! defined('EDD_DISABLE_EMAIL_REPORTS') ) {
 
-          $target_time_zone = new DateTimeZone( get_option('timezone_string') );
+          $timezone_string  = ! empty( get_option('timezone_string') ) ? get_option('timezone_string') : 'UTC';
+          $target_time_zone = new DateTimeZone( $timezone_string );
           $date_time = new DateTime('now', $target_time_zone);
 
             wp_schedule_event(
